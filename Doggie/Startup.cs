e087c352ebbfb409f94c34f72ecf42f9db9bd934
Nikolaj Mørup <nikolajmorup@gmail.com>
2019-03-04 08:45:21 +0100

    Add project files.

---
+++ Doggie/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Doggie.Startup))]
+namespace Doggie
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
