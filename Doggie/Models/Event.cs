005b4d4f506a780a016744d2614d6205567bcd25
Nikolaj Mørup <nikolajmorup@gmail.com>
2019-03-10 22:48:38 +0100

    Added display names

--- Doggie/Models/Event.cs
+++ Doggie/Models/Event.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Security.Permissions;
 using System.Web;
@@ -9,10 +10,16 @@ namespace Doggie.Models
     public class Event
     {
         public int Id { get; set; }
-        public string Location { get; set; }
-        public string DogOwnerName { get; set; }
-        public DateTime StartingTime { get; set; }
-        public int EstimatedTimeMinutes { get; set; }
 
+        public string Location { get; set; }
+
+        [Display(Name = "Dog Owner")]
+        public string DogOwnerName { get; set; }
+
+        [Display(Name = "Start Time")]
+        public DateTime StartingTime { get; set; }
+
+        [Display(Name = "Walking Time")]
+        public int EstimatedTimeMinutes { get; set; }
     }
 }
